32b522507b742a1828695365b8d6b84f7038a98b
VedanshTrivedi04 <165897916+VedanshTrivedi04@users.noreply.github.com>
2026-02-16 15:44:34 +0530

    Change API base URL to production endpoint

--- new frontend/aapatcall-react/src/api.js
+++ new frontend/aapatcall-react/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://on-call-mechanic.onrender.com";
 const API = axios.create({
   baseURL: `${API_BASE}/api/`,
 });
