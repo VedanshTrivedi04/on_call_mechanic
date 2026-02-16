dc5eed1aac66a7fd7a9b218c2be53c004af3ef92
VedanshTrivedi04 <165897916+VedanshTrivedi04@users.noreply.github.com>
2026-02-16 15:36:42 +0530

    Set up Django in ASGI configuration
    Initialize Django setup for ASGI application.

--- mechanic_ai/mechanic_ai/asgi.py
+++ mechanic_ai/mechanic_ai/asgi.py
@@ -1,13 +1,17 @@
-
-
 import os
+import django
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import core.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mechanic_ai.settings")
+
+django.setup()   # ⭐⭐⭐ VERY IMPORTANT ⭐⭐⭐
+
 print("🔥 ASGI FILE LOADED 🔥")
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
 
